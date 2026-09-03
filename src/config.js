6eabee4b98ec91b1e0d436001aea98bde61b46a8
Harshit Khandwalia <harshit8377@gmail.com>
2026-09-03 12:02:41 +0530

    redirects fix

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const API_BASE_URL = "https://portal-production-fc81.up.railway.app/api/credentials/";
 export const WALLET_API_BASE_URL = "https://portal-production-fc81.up.railway.app/api/wallet";
+// export const API_BASE_URL = "http://127.0.0.1:8003/api/credentials/";
+// export const WALLET_API_BASE_URL = "http://127.0.0.1:8003/api/wallet";
